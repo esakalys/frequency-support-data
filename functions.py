1f7f4b2fbb3fc326e448f857d5282d3b1c7b5946
Eividas <eividas.sakalys@pm.me>
2023-03-27 21:18:59 +0100

    study 0 done

--- functions.py
+++ functions.py
@@ -40,6 +40,27 @@ def importData(files, location, trimTime=True, single=False):
 
     return ds
 
+# Function used to import 3 phase simulation data stored in .mat files
+def import3phData(files, location, trimTime=True):
+
+    dir = os.getcwd()
+    ds = {}
+
+    for file in files:
+        mat = mat73.loadmat(f'{dir}/{location}/{file}')
+        a = mat['x'][1]
+        b = mat['x'][2]
+        c = mat['x'][3]
+        if trimTime:
+            t = [value-10 for value in mat['x'][0]]
+        else:
+            t = mat['x'][0]
+
+        data = {'Time': t, 'a': a, 'b': b, 'c': c}
+        ds[file] = data
+
+    return ds
+
 # Function for integrating the VSC power output
 def integrate(data, t1, t2, tStep=5e-6):
     sum = 0.0
