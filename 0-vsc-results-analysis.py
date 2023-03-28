52151fa8d4662502560dc8611389645a201dd697
Eividas <eividas.sakalys@pm.me>
2023-03-28 09:08:19 +0100

    study 0 update

--- 0-vsc-results-analysis.py
+++ 0-vsc-results-analysis.py
@@ -5,24 +5,21 @@ import matplotlib.pyplot as plt
 
 iqFiles = ['iq.mat', 'iqRef.mat']
 idFiles = ['id.mat', 'idRef.mat']
-vdFiles = ['vd.mat']
-vqFiles = ['vq.mat']
 vscPowerFiles = ['power-vsc.mat', 'power-vsc-ref.mat']
 vsc = ['vVsc.mat', 'iVsc.mat']
-grid = ['vGrid.mat', 'iGrid.mat']
 
 
-time = [4, 6]
+time = [4.5, 5.5]
 
 # Plotting q current
-ds = f.importData(iqFiles, 'data/v2-data/0-vsc-results')
+ds = f.importData(iqFiles, 'data/v2-data/0-vsc-results-unfiltered')
 
 plt.figure(figsize=(6, 4), dpi=400)
 
 plt.plot(ds[iqFiles[0]]['Time'], ds[iqFiles[0]]['Value'],
          color='#0789FA', linewidth=1.4, label='q Current')
 plt.plot(ds[iqFiles[1]]['Time'], ds[iqFiles[1]]['Value'],
-         color='#F10A0A', linewidth=0.8, label='q Current Reference', linestyle='--')
+         color='#F10A0A', linewidth=1.4, label='q Current Reference', linestyle='--')
 
 plt.grid(linewidth=0.3, linestyle='--', color='#E1E1E1')
 plt.title('VSC Results - q Current', pad=20)
@@ -34,14 +31,14 @@ plt.legend(loc='lower right')
 plt.savefig('plots/v2-plots/0-vsc-iq.jpg', dpi=400, bbox_inches='tight')
 
 # Plotting d current
-ds = f.importData(idFiles, 'data/v2-data/0-vsc-results')
+ds = f.importData(idFiles, 'data/v2-data/0-vsc-results-unfiltered')
 
 plt.figure(figsize=(6, 4), dpi=400)
 
 plt.plot(ds[idFiles[0]]['Time'], ds[idFiles[0]]['Value'],
          color='#0789FA', linewidth=1.4, label='d Current')
 plt.plot(ds[idFiles[1]]['Time'], ds[idFiles[1]]['Value'],
-         color='#F10A0A', linewidth=0.8, label='d Current Reference', linestyle='--')
+         color='#F10A0A', linewidth=1.4, label='d Current Reference', linestyle='--')
 
 plt.grid(linewidth=0.3, linestyle='--', color='#E1E1E1')
 plt.title('VSC Results - d Current', pad=20)
@@ -52,56 +49,23 @@ plt.ylim([-1000, 0])
 plt.legend(loc='upper right')
 plt.savefig('plots/v2-plots/0-vsc-id.jpg', dpi=400, bbox_inches='tight')
 
-# Plotting q voltage
-ds = f.importData(vqFiles, 'data/v2-data/0-vsc-results')
-
-plt.figure(figsize=(6, 4), dpi=400)
-
-plt.plot(ds[vqFiles[0]]['Time'], ds[vqFiles[0]]['Value'],
-         color='#0789FA', linewidth=1.4, label='q Voltage')
-
-plt.grid(linewidth=0.3, linestyle='--', color='#E1E1E1')
-plt.title('VSC Results - q Voltage', pad=20)
-plt.xlabel('Time, s')
-plt.ylabel('Voltage, V')
-plt.xlim(time)
-plt.ylim([-100, 1000])
-plt.legend(loc='lower right')
-plt.savefig('plots/v2-plots/0-vsc-vq.jpg', dpi=400, bbox_inches='tight')
-
-# Plotting d voltage
-ds = f.importData(vdFiles, 'data/v2-data/0-vsc-results')
-
-plt.figure(figsize=(6, 4), dpi=400)
-
-plt.plot(ds[vdFiles[0]]['Time'], ds[vdFiles[0]]['Value'],
-         color='#0789FA', linewidth=1.4, label='d Voltage')
-
-plt.grid(linewidth=0.3, linestyle='--', color='#E1E1E1')
-plt.title('VSC Results - d Voltage', pad=20)
-plt.xlabel('Time, s')
-plt.ylabel('Voltage, V')
-plt.xlim(time)
-plt.ylim([-100, 20000])
-plt.legend(loc='lower right')
-plt.savefig('plots/v2-plots/0-vsc-vd.jpg', dpi=400, bbox_inches='tight')
 
 # Plotting power
-ds = f.importData(vscPowerFiles, 'data/v2-data/0-vsc-results')
+ds = f.importData(vscPowerFiles, 'data/v2-data/0-vsc-results-unfiltered')
 
 plt.figure(figsize=(6, 4), dpi=400)
 
 plt.plot(ds[vscPowerFiles[0]]['Time'], ds[vscPowerFiles[0]]['Value'],
          color='#0789FA', linewidth=1.4, label='VSC Power')
 plt.plot(ds[vscPowerFiles[1]]['Time'], ds[vscPowerFiles[1]]['Value'],
-         color='#F10A0A', linewidth=0.8, label='VSC Power Reference', linestyle='--')
+         color='#F10A0A', linewidth=1.4, label='VSC Power Reference', linestyle='--')
 
 plt.grid(linewidth=0.3, linestyle='--', color='#E1E1E1')
 plt.title('VSC Results - Power', pad=20)
 plt.xlabel('Time, s')
 plt.ylabel('Power, MW')
 plt.xlim(time)
-#plt.ylim([-100, 100])
+plt.ylim([0, 30])
 plt.legend(loc='lower right')
 plt.savefig('plots/v2-plots/0-vsc-power.jpg', dpi=400, bbox_inches='tight')
 
@@ -109,7 +73,7 @@ phase_colors = ['#E60B09', '#34C62C', '#2C67C7']
 phase_time = [4.9, 5.1]
 
 # Plotting VSC Voltage
-ds = f.import3phData(vsc, 'data/v2-data/0-vsc-results')
+ds = f.import3phData(vsc, 'data/v2-data/0-vsc-results-unfiltered')
 
 plt.figure(figsize=(6, 4), dpi=400)
 
@@ -129,7 +93,7 @@ plt.ylim([-20000, 20000])
 plt.savefig('plots/v2-plots/0-vsc-voltage.jpg', dpi=400, bbox_inches='tight')
 
 # Plotting VSC Current
-ds = f.import3phData(vsc, 'data/v2-data/0-vsc-results')
+ds = f.import3phData(vsc, 'data/v2-data/0-vsc-results-unfiltered')
 
 plt.figure(figsize=(6, 4), dpi=400)
 
@@ -147,44 +111,3 @@ plt.ylabel('Current, A')
 plt.xlim(phase_time)
 plt.ylim([-1000, 1000])
 plt.savefig('plots/v2-plots/0-vsc-current.jpg', dpi=400, bbox_inches='tight')
-
-# Plotting Grid Voltage
-ds = f.import3phData(grid, 'data/v2-data/0-vsc-results')
-
-plt.figure(figsize=(6, 4), dpi=400)
-
-plt.plot(ds[grid[0]]['Time'], ds[grid[0]]['a'],
-         color=phase_colors[0], linewidth=1.4)
-plt.plot(ds[grid[0]]['Time'], ds[grid[0]]['b'],
-         color=phase_colors[1], linewidth=1.4)
-plt.plot(ds[grid[0]]['Time'], ds[grid[0]]['c'],
-         color=phase_colors[2], linewidth=1.4)
-
-plt.grid(linewidth=0.3, linestyle='--', color='#E1E1E1')
-plt.title('Grid Results - Voltage', pad=20)
-plt.xlabel('Time, s')
-plt.ylabel('Voltage, V')
-plt.xlim(phase_time)
-#plt.ylim([-100, 100])
-plt.savefig('plots/v2-plots/0-grid-voltage.jpg', dpi=400, bbox_inches='tight')
-
-# Plotting Grid Current
-ds = f.import3phData(grid, 'data/v2-data/0-vsc-results')
-
-plt.figure(figsize=(6, 4), dpi=400)
-
-plt.plot(ds[grid[1]]['Time'], ds[grid[1]]['a'],
-         color=phase_colors[0], linewidth=1.4)
-plt.plot(ds[grid[1]]['Time'], ds[grid[1]]['b'],
-         color=phase_colors[1], linewidth=1.4)
-plt.plot(ds[grid[1]]['Time'], ds[grid[1]]['c'],
-         color=phase_colors[2], linewidth=1.4)
-
-plt.grid(linewidth=0.3, linestyle='--', color='#E1E1E1')
-plt.title('Grid Results - Current', pad=20)
-plt.xlabel('Time, s')
-plt.ylabel('Current, A')
-plt.xlim(phase_time)
-plt.ylim([-400, 400])
-plt.savefig('plots/v2-plots/0-grid-current.jpg', dpi=400, bbox_inches='tight')
-
